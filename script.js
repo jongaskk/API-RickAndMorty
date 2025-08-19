1c864486fcf980386ec13fa9bb3b6265bcea8c17
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-08-19 11:31:17 -0300

    kjghriuh

---
+++ script.js
@@ -0,0 +1,18 @@
+const pageInput = document.getElementById("pageInput")
+const searchBtn = document.getElementById("searchBtn")
+const resultsDiv = document.getElementById("results")
+
+async function fetchCharacters(page){
+    resultsDiv.innerHTML = "<p>Carregando..</p>"
+
+    try {
+        const response = await fetch(`https://rickandmortyapi.com/api/character`)
+        const data = await response.json()
+        // console.log(data)
+    } catch (error) {
+          resultsDiv.innerHTML = "<p>Erro ao buscar personagens!</p>"
+    }
+}
+
+
+fetchCharacters(1)
